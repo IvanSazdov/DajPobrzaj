15a6428b77c06ae00993efccd2503630f42f3f94
Ivan Sazdov <ivan.sazdov@yahoo.com>
2023-08-22 15:08:47 +0200

    Added wait time

--- CarDetection/main.py
+++ CarDetection/main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from ultralytics import YOLO
-from shapely.geometry.polygon import Polygon, Point
+from shapely.geometry import Polygon, Point
 
 # def RGB(event, x, y, flags, param):
 #     if event == cv2.EVENT_MOUSEMOVE:
@@ -14,13 +14,16 @@ from shapely.geometry.polygon import Polygon, Point
 
 model = YOLO('models/yolov8x.pt')
 
-video_path = "media/traffic_short.mp4"
+video_path = "media/premin.mp4"
 cap = cv2.VideoCapture(video_path)
 
 area_premin = [(581, 298), (345, 448), (1618, 456), (1581, 303)]
 area_traffic = [(168, 384), (91, 486), (729, 547), (706, 416)]
 
-polygon = Polygon(area_traffic)
+polygon = Polygon(area_premin)
+dict = {}
+
+print(dict.get(1))
 cars = set()
 frame_counter = 0
 # Loop through the video frames
@@ -48,13 +51,16 @@ while cap.isOpened():
                 cx = int(x1 + x2) // 2
                 cy = int(y1 + y2) // 2
                 point = [Point(cx, cy), int(box.id)]
-                # point = Point(cx,cy)
+
                 annotated_frame = cv2.circle(annotated_frame, (cx, cy), 3, thickness=1, color=(0, 255, 0))
 
                 if polygon.contains(point[0]):
+                    if dict.get(point[1]) is None:
+                        dict[point[1]] = 0
+                    dict[point[1]] = dict[point[1]] + 1
                     cars.add(point[1])
 
-            cv2.polylines(annotated_frame, [np.array(area_traffic, np.int32)], True, (255, 255, 0), 3)
+            cv2.polylines(annotated_frame, [np.array(area_premin, np.int32)], True, (255, 255, 0), 3)
 
             # Display the annotated frame
             cv2.putText(annotated_frame,
@@ -64,6 +70,10 @@ while cap.isOpened():
                         5,
                         (255, 0, 0),
                         5)
+            newX = int(annotated_frame.shape[1] * 0.75)
+            newY = int(annotated_frame.shape[0] * 0.75)
+            dim = (newX, newY)
+            annotated_frame = cv2.resize(annotated_frame, dim, interpolation=cv2.INTER_AREA)
             cv2.imshow("Traffic", annotated_frame)
 
             # Break the loop if 'q' is pressed
@@ -76,6 +86,10 @@ while cap.isOpened():
         continue
 
 # Release the video capture object and close the display window
-print(len(cars))
+print("Number of cars:", len(cars))
+print("Frame counter:", frame_counter)
+print(dict)
+
+
 cap.release()
 cv2.destroyAllWindows()
